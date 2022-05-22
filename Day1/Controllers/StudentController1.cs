23ec69bef54684a8423ed768cf0de1248aeed358
Ehsan <az202719@gmail.com>
2022-05-22 22:19:53 +0200

    FirstCommit

---
+++ Day1/Controllers/StudentController1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Day1.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            StudentList context = new StudentList();
+            List<Student> studentList2 = context.StudentsFunc();
+            return View("AllStudents", studentList2);
+        }
+
+        public IActionResult studentDetails(int id)
+        {
+            StudentList context = new StudentList();
+            List<Student> ProductList = context.StudentsFunc();
+            Student student = ProductList.FirstOrDefault(p => p.ID == id);
+            return View("GetDetails", student);
+        }
+    }
+}
---
+++ Day1/Models/StudentList.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Day1.Models
+{
+    public class StudentList
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student (){ID=1, Name="Hossam", Address="assuit", Image="img4.jpg"},
+            new Student (){ID=2, Name="Ahmed", Address="assuit", Image="img5.jpg"},
+            new Student (){ID=3, Name="alaa", Address="assuit", Image="img6.jpg"}
+
+        };
+
+        public List<Student> StudentsFunc()
+        {
+            return students;    
+        }
+    }
+}
